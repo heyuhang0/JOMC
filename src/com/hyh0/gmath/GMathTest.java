ae0e840575fad04c3d2257ca255563ef6ee848c4
hyh0 <heyuhang0@outlook.com>
2017-09-07 23:24:42 +0800

    加入了矩阵运算速度的测试程序

--- src/com/hyh0/gmath/GMathTest.java
+++ src/com/hyh0/gmath/GMathTest.java
@@ -6,24 +6,35 @@ public class GMathTest {
     
     public static void main(String[] args) {
         GMath gpu = new GMath();
+        speedTest(1000, 1000, 1000, gpu);
         
-        GMatrix mA = gpu.newGMatrix(10000, 1000);
-        GMatrix mB = gpu.newGMatrix(10000, 1000);
-        gpu.fillMatrixRandomly(mA, -5, 5);
+        speedTest(10, 2000, 2000, gpu);
+        speedTest(2000, 10, 2000, gpu);
+        speedTest(2000, 2000, 10, gpu);
         
-        Tools.resetTimer();
-        gpu.sigmoid(mA, mB);
-        gpu.finish();
-        Tools.showTimer();
-        double a = 100;
-        for(int i = 0; i < 10000000; i++) {
-            a = sigmoid((double)i/1000000);
-        }
-        Tools.showTimer();
+        speedTest(10, 10, 50000, gpu);
+        speedTest(50000, 10, 10, gpu);
+        speedTest(10, 50000, 10, gpu);
+        
         gpu.release();
     }
     
-    private static double sigmoid(double x) {
-        return 1 / (1 + Math.exp(-x));
-    }
+    static void speedTest(int m, int n, int p, GMath gpu) {
+        GMatrix mA = gpu.newGMatrix(m, n);
+        GMatrix mB = gpu.newGMatrix(n, p);
+        GMatrix mC = gpu.newGMatrix(m, p);
+        
+        gpu.fillMatrixRandomly(mA, -5, 5);
+        gpu.fillMatrixRandomly(mB, -5, 5);
+        
+        Tools.resetTimer();
+        gpu.multiply(mA, mB, mC);
+        gpu.finish();
+        System.out.print("m=" + m + " n=" + n + " p=" + p + " : ");
+        Tools.showTimer();
+        
+        mA.release();
+        mB.release();
+        mC.release();
+    } 
 }
